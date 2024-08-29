86e1d7f1bf0d1255b9f2ee3026f97dde6fed6aec
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2024-08-29 14:40:07 +0700

    update the urls

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,9 @@ app_name = 'app'
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing'),
     path('input-nilai', InputScoreView.as_view(), name='score-input'),
-
+    path('registration/<slug>', RegistrationPhaseView.as_view(), name='registration-phase'),
     path('reset-password', ForgotPasswordView.as_view(), name='reset-password'),
-
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('profile/create', CreateProfileView.as_view(), name='profile-create'),
+    path('profile/update/<slug>', UpdateProfileView.as_view(), name='profile-update')
 ]
